b60fa2cd3731ea00ee994c9df5ab5d865371815b
田秾恺 <66242712+T990820@users.noreply.github.com>
2021-03-15 00:28:39 -0500

    Add files via upload

---
+++ C/6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+    int i=0,j=0;
+    for(i;i<=3;i++)
+    {
+        for(j;j<=2*i-1;j++)
+        {
+            printf(" ");
+        }
+        j=0;
+        printf("*****\n");
+    }
+}
+
+    
